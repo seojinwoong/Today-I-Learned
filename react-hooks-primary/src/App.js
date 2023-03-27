6eb080396fb46f12be4ac8a495727f2e9a068778
seojinwoong <tjwlsdnd12@naver.com>
2023-03-28 02:19:09 +0900

    useEffect cleanup

--- react-hooks-primary/src/App.js
+++ react-hooks-primary/src/App.js
@@ -1,43 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react'
+import Timer from './component/Timer';
 
 const App = () => {
-  const [count, setCount] = useState(1);
-  const [name, setName] = useState('');
-
-  const handleCountUpdate = () => {
-    setCount(count + 1);
-  }
-
-  const handleInputChange = (e) => {
-    setName(e.target.value);
-  }
-
-  // 렌더링마다 매번 실행됨 
-  useEffect(() => {
-    console.log('렌더링')
-  });
-
-  // 마운트 될때 + [name] 변경될때만 실행
-  useEffect(() => {
-    console.log('name이 변화')
-  }, [name]);
-
-  // 마운트 될때 + [count] 변경될때만 실행
-  useEffect(() => {
-    console.log('count가 변화')
-  }, [count]);
-
-  // 마운트 될때만 처음에만 실행
-  useEffect(() => {
-    console.log('마운팅');
-  }, []);
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div>
-      <button onClick={handleCountUpdate}>update</button>
-      <span>count : {count}</span>
-      <input type="text" value={name} onChange={handleInputChange}/>
-      <span>name: {name}</span>
+      { showTimer && <Timer /> }
+      <button onCLick={() => setShowTimer(!showTimer)}>toggle Timer</button>
     </div>
   )
 }

b7be4b7ad371e10dadd70c6596d171953e1c0612
seojinwoong <tjwlsdnd12@naver.com>
2023-04-06 16:48:17 +0900

    useReducer 기초1

--- react-hooks-primary/src/App.js
+++ react-hooks-primary/src/App.js
@@ -1,25 +1,41 @@
-import React, { useState, useCallback } from 'react';
-import Box from './Box';
+import React, { useState, useReducer } from 'react';
+
+// reducer - state를 업데이트 하는 역할 (은행)
+// dispatch - state 업데이트를 위한 요구
+// action - 요구의 내용
+
+const ACTION_TYPES = {
+  deposit: 'deposit',
+  withdraw: 'withdraw'
+}
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case ACTION_TYPES.deposit: 
+      return state + Number(action.payload);
+    case ACTION_TYPES.withdraw:
+      return state - Number(action.payload);
+    default:
+      return state;
+  }
+}
 
 const App = () => {
-  const [size, setSize] = useState(100);
-  const [isDark, setIsDark] = useState(false);
-
-  const createBoxStyle = useCallback(() => {
-    return {
-      backgroundColor: 'pink',
-      width: `${size}px`,
-      height: `${size}px`
-    }
-  }, [size]);
+  const [number, setNumber] = useState(0);
+  const [money, dispatch] = useReducer(reducer, 0);
 
   return (
-    <div style={{
-      background: isDark ? 'black' : 'white'
-    }}>
-      <input type="number" value={size} onChange={(e) => setSize(e.target.value)} />
-      <button onClick={() => setIsDark(!isDark)}>toggle theme</button>
-      <Box createBoxStyle={createBoxStyle} />
+    <div>
+      <h2>useReducer은행에 오신것을 환영합니다</h2>    
+      <p>잔고 : {money}원</p>
+      <input type="number" value={number} onChange={e => setNumber(e.target.value)} step="1000"/>
+      <button onClick={() => {
+        dispatch({type: ACTION_TYPES.deposit, payload: number})
+      }}>예금</button>
+      <button onClick={() => {
+        dispatch({type: ACTION_TYPES.withdraw, payload: number})
+      }}>출금</button>
     </div>
   )
 }

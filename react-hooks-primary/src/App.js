279820c93761efa825a4aea53b3feb4dcd5c38d4
seojinwoong <tjwlsdnd12@naver.com>
2023-04-06 14:49:10 +0900

    useCallback 기초1

--- react-hooks-primary/src/App.js
+++ react-hooks-primary/src/App.js
@@ -1,31 +1,24 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const App = () => {
   const [number, setNumber] = useState(0);
-  const [isKorea, setIsKorea] = useState(true);
+  const [toggle, setToggle] = useState(true);
 
-  const location = useMemo(() => {
-    return {
-      country: isKorea ? '한국' : '외국'
-    }
-  }, [isKorea]);
-  
-  // {
-  //   country: isKorea ? '한국' : '외국'
-  // }
+  const someFunction = useCallback(() => {
+    console.log(`someFunc : number : ${number}`);
+    return;
+  }, [number]);
 
   useEffect(() => {
-    console.log('useEffect 호출');
-  }, [location]);
+    console.log(`someFunc이 변경되었습니다.`);
+  }, [someFunction]);
 
   return (
     <div>
-      <h2>하루에 몇끼 먹어요?</h2>
       <input type="number" value={number} onChange={(e) => setNumber(e.target.value)}/>
-      <hr />
-      <h2>어느 나라에 있어요?</h2>
-      <p>나라 : {location.country}</p>      
-      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+      <button onClick={() => setToggle(!toggle)}>{toggle.toString()}</button>
+      <br />
+      <button onClick={someFunction}>Call someFunc</button>
     </div>
   )
 }

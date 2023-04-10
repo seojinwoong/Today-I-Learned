9925f9ca2946aaf9cc50eb6c0bb46019ca3c6e43
seojinwoong <tjwlsdnd12@naver.com>
2023-04-10 16:37:03 +0900

    커스텀 훅 기초1

--- react-hooks-primary/src/App.js
+++ react-hooks-primary/src/App.js
@@ -1,64 +1,17 @@
-import React, { useReducer, useState } from 'react';
-import Student from './Student';
+import { useInput } from './useInput';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'add-student':
-      const name = action.payload.name;
-      const newStudent = {
-        id: Date.now(),
-        name,
-        isHere: true
-      }
-      return {
-        count: state.count + 1,
-        students: [...state.students, newStudent],
-      }
-    case 'delete-student': 
-      return {
-        count: state.count - 1,
-        students: state.students.filter(student => student.id !== action.payload.id)
-      }
-    case 'mark-student': 
-      return {
-        count: state.count,
-        students: state.students.map(student => {
-          if (student.id === action.payload.id) {
-            return {...student, isHere: !student.isHere}
-          }
-          return student;
-        })
-      }
-    default:
-      return state
-  }
-};
-
-const initialState = {
-  count: 0,
-  students: []
+function displayMessage(message) {
+  alert(message);
 }
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [studentInfo, dispatch] = useReducer(reducer, initialState);
+  const [inputValue, handleChange, handleSubmit] = useInput('', displayMessage);
 
   return (
     <div>
-      <h1>출석부</h1>
-      <p>총 학생 수 : {studentInfo.count}</p>
-      <input 
-        type="text" 
-        placeholder='이름을 입력해주세요'
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={() => {
-        dispatch({type: 'add-student', payload: {name}})
-      }}>추가</button>
-      {studentInfo.students.map(student => {
-        return (<Student key={student.id} name={student.name} dispatch={dispatch} id={student.id} isHere={student.isHere}/>)
-      })}
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange}/>
+      <button onClick={handleSubmit}>확인</button>
     </div>
   )
 }

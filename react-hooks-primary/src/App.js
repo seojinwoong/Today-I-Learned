23d5377158f904254004b4138c61982d0022d04b
seojinwoong <tjwlsdnd12@naver.com>
2023-04-11 14:32:48 +0900

    useCallback + React.memo 조합으로 컴포넌트 최적화하기

--- react-hooks-primary/src/App.js
+++ react-hooks-primary/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useCallback } from 'react';
 import Child from './Child';
 
 const App = () => {
@@ -8,18 +8,18 @@ const App = () => {
     setParentAge(parentAge + 1);
   }
 
-  console.log('부모 컴포넌트가 랜더링 되었어요');
-
-  const name = useMemo(() => {
-    return { lastName: '홍', firstName: '길동' };
+  const tellMe = useCallback(() => {
+    console.log('길동아 사랑해');
   }, []);
+
+  console.log('부모 컴포넌트가 랜더링 되었어요');
 
   return (
     <div style={{ border: '2px solid navy', padding: '10px' }}>
         <h1>부모</h1>
         <p>age : {parentAge}</p>
         <button onClick={incrementParentAge}>부모 나이 증가</button>
-        <Child name={name}/>
+        <Child name={"홍길동"} tellMe={tellMe}/>
     </div>
   )
 }
--- react-hooks-primary/src/Child.js
+++ react-hooks-primary/src/Child.js
@@ -1,13 +1,13 @@
 import React, { memo } from 'react'
 
-const Child = ({name}) => {
+const Child = ({name, tellMe}) => {
 
   console.log('자녀 컴포넌트 랜더링 되었습니다');
   return (
     <div style={{ border: '2px solid powderblue', padding: '10px' }}>
         <h3>자녀</h3>
-        <p>성: {name.lastName}</p>
-        <p>이름: {name.firstName}</p>
+        <p>이름 : {name}</p>
+        <button onClick={tellMe}>엄마 나 사랑해?</button>
     </div>
   )
 }
